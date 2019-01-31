3d9dcea883122a7a4d86a6e10061265719eb9c7b
Blanche Faivre <blanche.faivre@etu.unice.fr>
2019-01-31 09:25:18 +0100

    commit 1

--- src/ButtonWidget.js
+++ src/ButtonWidget.js
@@ -1,10 +1,11 @@
+/*eslint-disable*/
 import $ from 'jquery/dist/jquery.min';
 
-import TUIOWidget from 'tuiomanager/core/TUIOWidget';
+// import TUIOWidget from 'tuiomanager/core/TUIOWidget';
 import ElementWidget from 'tuiomanager/widgets/ElementWidget/ElementWidget';
 
 
-class ButtonWidget extends TUIOWidget {
+class ButtonWidget extends ElementWidget {
     /**
      * ImageWidget constructor.
      *
@@ -14,8 +15,8 @@ class ButtonWidget extends TUIOWidget {
      * @param {number} width - ImageWidget's width.
      * @param {number} height - ImageWidget's height.
      */
-    constructor(x, y, width, height, imgSrc) {
-        super(x, y, width, height);
+    constructor(x, y, width, height, rotation, scale, imgSrc) {
+        super(x, y, width, height, rotation, scale);
         if (new.target === ElementWidget) {
             throw new TypeError('ElementWidget is an abstract class. It cannot be instanciated');
         }
--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+/*eslint-disable*/
+
 /**
  * @author Christian Brel <ch.brel@gmail.com>
  * @author Vincent Forquet
@@ -23,7 +25,7 @@ const buildApp = () => {
 
     });
     // $('#app').load('src/test.html');
-    const button = new ButtonWidget(100, 150, 110, 110, 0, 1, 'assets/example-health/candies.png', 'B3', 'C9', '38');
+    const button = new ButtonWidget(100, 150, 900, 900, 0, 1, 'assets/example-health/candies.png');
     button.addTo('#app');
     // const puz = new ImageElementWidget(50, 200, 200, 200, 0, 2, 'assets/example-puzzle/1.png', 'B3', 'C9', '38');
     // puz.addTo('#example-container');
