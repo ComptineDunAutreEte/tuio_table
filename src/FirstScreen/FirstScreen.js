29e9b4f3798e0b6de8f497d08000b2c8f93e2f3a
AlexGreau <alexgreau@live.fr>
2019-01-31 01:08:08 +0100

    merge with web version

---
+++ src/FirstScreen/FirstScreen.js
@@ -0,0 +1,84 @@
+/* eslint-disable */
+
+class FirstScreen {
+    constructor() {
+        this.playerCount = 2;
+        this.difficultyLevel = 1; // 1-3 => facile difficile
+        this.id = "firstScreen"
+        this.containerID = "app";
+        this.containerClass = "container-fluid d-flex h-100";
+    }
+
+    populate(id) {
+        const pid = "#" + id;
+        const str = '<div id="firstScreen" class="row align-self-center align-items-center w-100"> </div>';
+        $(pid).append(str);
+        this.createPlayerCountCOL(this.id);
+        this.createConfirmBtn(this.id);
+        this.createDifficultyLevelCOL(this.id);
+        document.getElementById(this.containerID).className = this.containerClass + " " + "firstScreenBackground";
+    }
+
+    createPlayerCountCOL(parentID) {
+        const pid = "#" + parentID;
+        // contenu
+        $(pid).append('<div id="nPlayersHalf" class="col"> </div>');
+        $('#nPlayersHalf').append('<div class="row justify-content-center" id="playerCountHalfRow"> </div>');
+
+        const btnsIDs = ['2playersBtn', '4playersBtn', '6playersBtn'];
+        const btnsTexts = ['2 players', '4 players', '6 players'];
+        const colors = ["secondary", "secondary", "secondary"];
+        this.createRadioBtnGroup(btnsIDs, btnsTexts, colors, 'playerCountHalfRow');
+    }
+
+    createDifficultyLevelCOL(parentID) {
+        const pid = "#" + parentID;
+        // contenu
+        $(pid).append('<div id="difficultyHalf" class="col"> </div>');
+        $('#difficultyHalf').append('<div class="row justify-content-center" id="diffHalfRow"> </div>');
+
+        const btnsIDs = ['easyBtn', 'mediumBtn', 'hardBtn'];
+        const btnsTexts = ['facile', 'moyen', 'difficile'];
+        const colors = ["success", "warning", "danger"];
+        this.createRadioBtnGroup(btnsIDs, btnsTexts, colors, 'diffHalfRow');
+    }
+
+    createRadioBtnGroup(ids, texts, colors, parentID) {
+        console.log("creating radio buttons...")
+        const pid = "#" + parentID;
+        const selfIDstr = parentID + "radioGroup";
+        const selfID = "#" + selfIDstr;
+        const selfstring = '<div id="' + selfIDstr + '" class="btn-group-lg btn-group-toggle" data-toggle="buttons"></div>'
+        $(pid).append(selfstring);
+        for (let i = 0; i < ids.length; i++) {
+            $(selfID).append('<label class="btn btn-' + colors[i] + '"> \
+            <input type="radio" name="options" id=' + ids[i] + 'autocomplete="off" checked>' + texts[i] + '</label>');
+        }
+    }
+
+    createConfirmBtn(parentID) {
+        const pid = "#" + parentID;
+        $(pid).append('<button id="confirmFirstScreenBtn" type="button" class="btn btn-info btn-circle btn-xxl"><i class="fa fa-check"></i></button>');
+    }
+
+    // getters and setters
+    setPlayerCount(i) {
+        // add verification ?
+        this.playerCount = i;
+    }
+
+    getPlayerCount() {
+        return this.playerCount;
+    }
+
+    setDifficultyLevel(i) {
+        // add verification ?
+        this.difficultyLevel = i;
+    }
+
+    getDifficultyLevel() {
+        return this.difficultyLevel;
+    }
+
+
+} export default FirstScreen;
--- src/index.js
+++ src/index.js
@@ -8,17 +8,32 @@ import TUIOManager from 'tuiomanager/core/TUIOManager';
 import ButtonWidget from './ButtonWidget';
 import ImageElementWidget from 'tuiomanager/widgets/ElementWidget/ImageElementWidget/ImageElementWidget';
 import buildMenu from './menu';
+
+import FirstScreen from './FirstScreen/FirstScreen'
 /* * TUIOManager starter * */
 const tuioManager = new TUIOManager();
 tuioManager.start();
 
+/** Main container infos **/
+const mainContainerID = "app";
+const mainContainerClass = "container-fluid d-flex h-100";
+
 /* * App Code * */
 
+/* test of button widget */
 const buildApp = () => {
     const button = new ButtonWidget(0, 0, 100, 100, 'assets/example-health/candies.png');
     button.addTo('#app');
 };
 
+function loadFirstScreen() {
+    console.log("loading First Screen...");
+    const firstScreen = new FirstScreen();
+    firstScreen.populate(mainContainerID);
+    console.log("finished loading First Screen.");
+}
+
 $(window).ready(() => {
-    buildApp();
-});
+    console.log("Document well loaded");
+    loadFirstScreen();
+})
