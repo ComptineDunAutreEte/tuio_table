2dd22effae933979e09b6373caee9b95806426ed
AlexGreau <alexgreau@live.fr>
2019-02-01 09:22:39 +0100

    background try

--- src/FormationScreen/FormationScreen.js
+++ src/FormationScreen/FormationScreen.js
@@ -21,6 +21,7 @@ class FormationScreen {
     inflate(){
         $('#app').append('<h1 id="textEquipeB" style="width: 100%; height: 20%; position: relative; top:28%; left:100%;transform: rotate(180deg)" ></h1>\
         <h1 id="textEquipeR" style="width: 100%;height: 20%; position: relative; top:30%; left:100%;" > </h1>');
+        document.getElementById('app').className = "container-fluid d-flex h-100 FromationScreen"
     }
 
     buildFormation() {
