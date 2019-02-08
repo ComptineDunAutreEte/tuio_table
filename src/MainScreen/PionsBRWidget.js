1ec3eefe25b45cbb6fb850e58dce17c434ffd25f
Blanche Faivre <blanche.faivre@etu.unice.fr>
2019-02-08 11:03:56 +0100

    pawnMoved Update

--- src/MainScreen/PionsBRWidget.js
+++ src/MainScreen/PionsBRWidget.js
@@ -135,7 +135,7 @@ class PionsBRWidget extends PionsWidget {
       //  }
     }
 
-    voisIsNotPanw(vois){
+    voisIsNotPawn(vois){
         for (var i = 0; i < PionsWidget.nbPionsBR; i++){
             if (vois.idp === PionsWidget.listePionsBR[i].place) {
                 return false;
@@ -165,15 +165,17 @@ class PionsBRWidget extends PionsWidget {
             const intY = this.internY;
             const vois = this.voisins;
             for (var i = 0; i < nbV; i++) {
-                if ((intX <= vois[i].internX + 20 - 14.5) &&
+                //Si le pion est relaché près d'un de ses voisins
+                if (((intX <= vois[i].internX + 20 - 14.5) &&
                     (intX >= vois[i].internX - 20 - 14.5) &&
                     (intY <= vois[i].internY + 20 - 14.5) &&
-                    (intY >= vois[i].internY - 20 - 14.5)){
+                    (intY >= vois[i].internY - 20 - 14.5))&&(this.voisIsNotPawn(vois[i]))){
+                    //On remet les voisins nomraux
                     for (var j = 0; j < this.nbVoisins; j++){
                         this.voisins[j]._domElem.attr('src', 'assets/MainScreen/pionN.png');
                         this.voisins[j].src = 'assets/MainScreen/pionN.png';
                     }
-                    /*Changement coordonnées du pion*/
+                    //Changement coordonnées du pion qui prend les coordonnées de son voisin
                     this._domElem.css('left', `${this.voisins[i].internX - 14.5}px`);
                     this._domElem.css('top', `${this.voisins[i].internY - 14.5}px`);
                     this.internX = this.voisins[i].internX - 14.5;
@@ -184,7 +186,7 @@ class PionsBRWidget extends PionsWidget {
                     this.nbVoisins = this.voisins[i].nbVoisins;
                     this.voisins = this.voisins[i].voisins;
 
-                    /*Changement coordonnées du ballon*/
+                    //Changement coordonnées du ballon si le pion le possède
                     if (this.aLeBallon){
                         if (this.src === 'assets/MainScreen/pionB.png') {
                             this.ballon._domElem.css('left', `${this.internX + this.width + 5}px`);
@@ -203,8 +205,11 @@ class PionsBRWidget extends PionsWidget {
                     }
                     nbV = i;
                     deplace = true;
+                    if (this.src === 'assets/MainScreen/pionB.png'){this.pawnTouched("blue");}
+                    else {this.pawnTouched("red");}
                 }
             }
+            //Si le pion n'a pas été placé sur un voisin, ou si la case est il revient à sa place
             if (!deplace) {
                 console.log("iiccii");
                 this._domElem.css('left', `${this.mx}px`);
