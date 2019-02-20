5f3d045bf2fd2a225db7c356f73f25a3eb73dd3a
AlexGreau <alexgreau@live.fr>
2019-02-20 17:48:14 +0100

    fin debug animation

--- src/FirstScreen/FirstScreen.js
+++ src/FirstScreen/FirstScreen.js
@@ -32,29 +32,24 @@ class FirstScreen {
         const mainColID = "mainCol";
         // main COL
         $(pid).append('<div id="' + mainColID + '" class="col h-100 w-100 align-items-center"> </div>');
-        /*
+
         $(pid).append('<button id="add_mock" class="testButton">Request Player from server<button>');
         $(pid).append('<button id="scores_mock" class="testButton2" >Request Scores from server<button>');
-        */
+
         // Top row
         $("#" + mainColID).append('<div id="' + topRowID + '" class="row justify-content-center pb-5 topRow"> </div>');
-
-        //$("#" + topRowID ).append();
         $("#" + topRowID).append('<div id="topDeck" class="card-deck"> </div>');
-
         // middle row
         $("#" + mainColID).append('<div id="' + middleRowID + '" class="row justify-content-center mt-10"> </div>');
         this.createConfirmBtn(middleRowID);
-
         // Bottom row
         $("#" + mainColID).append('<div id="' + botRowID + '" class="row justify-content-center pt-5 botRow"> </div>');
-        //$("#" + botRowID ).append();
         $("#" + botRowID).append('<div id="botDeck" class="card-deck"> </div>');
 
-        /*
+
         this.initMockAddButton();
         this.initMockScoresButton();
-        */
+
     }
 
     createConfirmBtn(parentID) {
@@ -68,14 +63,10 @@ class FirstScreen {
                 that.observer.finishedFirstscreen();
             }
         };
-        /*
-        const message = this.playerCount + "," + this.difficultyLevel;
-        console.log("should be : " + message)
-        */
     }
 
 
-    createDifficultyLevelStepper() {}
+    createDifficultyLevelStepper() { }
 
     createPlayerCountCOL(parentID) {
         const pid = parentID;
@@ -114,17 +105,17 @@ class FirstScreen {
     }
 
     createRadioBtnGroup(ids, texts, colors, parentID) {
-            const pid = "#" + parentID;
-            const selfIDstr = parentID + "radioGroup";
-            const selfID = "#" + selfIDstr;
-            const selfstring = '<div id="' + selfIDstr + '" class="btn-group-lg btn-group-toggle" data-toggle="buttons"></div>';
-            $(pid).append(selfstring);
-            for (let i = 0; i < ids.length; i++) {
-                $(selfID).append('<label id="' + ids[i] + '" class="btn btn-' + colors[i] + '"> \
+        const pid = "#" + parentID;
+        const selfIDstr = parentID + "radioGroup";
+        const selfID = "#" + selfIDstr;
+        const selfstring = '<div id="' + selfIDstr + '" class="btn-group-lg btn-group-toggle" data-toggle="buttons"></div>';
+        $(pid).append(selfstring);
+        for (let i = 0; i < ids.length; i++) {
+            $(selfID).append('<label id="' + ids[i] + '" class="btn btn-' + colors[i] + '"> \
             <input type="radio" name="options" autocomplete="off" checked>' + texts[i] + '</label>');
-            }
         }
-        /* MISC */
+    }
+    /* MISC */
     initMockAddButton() {
         //$("#" + botRowID ).append();
         document.getElementById("add_mock").onclick = () => {
@@ -143,7 +134,7 @@ class FirstScreen {
     }
 
     addPlayerCard(team, nem) {
-        this.playerCount ++;
+        this.playerCount++;
         let animationClass, teamClass, whereID;
         if (team === "A") {
             animationClass = "slideUp";
@@ -161,6 +152,13 @@ class FirstScreen {
                                 <p class="card-text">' + nem + '</p>\
                             </div>\
                         </div>');
+        if (this.playerCount % 2 === 0) {
+            console.log("confirm button to pulsating");
+            this.setPulsating("confirmFirstScreenBtn", true);
+        } else {
+            console.log("confirm button to non-pulsating");
+            this.setPulsating("confirmFirstScreenBtn", false);
+        }
     }
 
 
