f01e069cbe87f119556652a3148a95b85da1c5f3
AlexGreau <alexgreau@live.fr>
2019-02-20 19:55:25 +0100

    Creation ScoreScreen

---
+++ src/ScoreScreen/ScoreScreen.js
@@ -0,0 +1,16 @@
+/* eslint-disable */
+
+class ScoreScreen {
+    // modal pour afficher les highscores
+    constructor(pid, scores){
+        this.pid = pid;
+        this.scores = scores;
+    }
+
+    populate(){
+        // gonfle les cartes
+        const PID = "#" + this.pid;
+
+    }
+
+} export default ScoreScreen;
