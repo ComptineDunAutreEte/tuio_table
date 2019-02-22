de7e0b445ec84cffb273a15303a15f6efd24a2f7
haneul <kehoecicco@live.fr>
2019-02-22 08:45:48 +0100

    hello

--- src/LifeCycle/Lifecycle.js
+++ src/LifeCycle/Lifecycle.js
@@ -298,7 +298,7 @@ class Lifecycle {
         this.clearScreen();
         $('#app').className = this.containerClass;
         console.log("premier appel : " + this.premierAppel);
-       // this.startingTeam = "red";
+        // this.startingTeam = "red";
         const mainScreen = new MainScreen(WINDOW_WIDTH, WINDOW_HEIGHT, this, PionsWidget.valuesSaved, this.startingTeam, this.premierAppel);
         this.premierAppel = false;
         this.actualScreen = mainScreen;
@@ -395,7 +395,8 @@ class Lifecycle {
         });
 
         client.getSocket().on('terminer', (msg) => {
-            this.loadMainScreen('red');
+            this.loadMainScreen('rouge');
+            //this.loadMainScreen(teamToplay) 
         });
 
         client.getSocket().on('all-answered', (msg) => {
